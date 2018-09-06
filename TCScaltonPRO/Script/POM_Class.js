a5dd8f46489aca9f03249c60c360366783c9ea7d
MariaSolomonLouis <34271140+MariaSolomonLouis@users.noreply.github.com>
2018-09-06 12:42:37 +0530

    Print Message

--- TCScaltonPRO/Script/POM_Class.js
+++ TCScaltonPRO/Script/POM_Class.js
@@ -1,1 +1,23 @@
-﻿
+﻿class POM_Class
+{
+	construnctor(){
+		initiallizeLocators();
+	}
+	
+	initiallizeLocators()
+	{
+		this.TestVariable = "Hello";
+	}
+	
+	print(sMsg){
+		try{
+			Log.Message(sMsg);
+		}
+		catch(e)
+		{
+			throw new Error(e.message);
+		}
+	}
+}
+
+module.exports = POM_Class;
--- TCScaltonPRO/Script/Test_Class.js
+++ TCScaltonPRO/Script/Test_Class.js
@@ -1,1 +1,15 @@
-﻿
+﻿var POM_Class = require("POM_Class");
+
+function Test1(){
+	try{
+		pom_class = new POM_Class();
+		
+		var sMessage = pom_class.TestVariable;
+		
+		pom_class.print(sMessage);
+	}
+	catch(e)
+	{
+		Log.Message(e.message);
+	}
+}
